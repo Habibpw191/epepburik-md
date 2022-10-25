dce59eaf8093674f7d65eb0dbc3a42c8482e6e65
Habibpw191 <116631223+Habibpw191@users.noreply.github.com>
2022-10-25 21:00:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,12 +16,12 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['62895604670507']
-global.ownernomer = "62895604670507"
-global.premium = ['62895604670507','6281328144300']
-global.packname = 'Zenss'
-global.author = 'Cuyy'
-global.sessionName = 'zenss'
+global.owner = ['6289528935082']
+global.ownernomer = "6289528935082"
+global.premium = ['6289528935082','6289528935082']
+global.packname = 'Zii'
+global.author = 'Zii'
+global.sessionName = 'Zii'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
 global.mess = {
